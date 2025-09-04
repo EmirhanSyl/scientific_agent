b73a5d08eb8d721f7f050ad5a2b2c4469ea29cfe
Emirhan Soylu <61618968+EmirhanSyl@users.noreply.github.com>
2025-09-05 02:24:39 +0300

    Fix

--- rag_agent/agent.py
+++ rag_agent/agent.py
@@ -8,8 +8,8 @@ from langchain_community.vectorstores import FAISS
 from langchain.text_splitter import CharacterTextSplitter
 from langchain.chains import RetrievalQA
 from langchain.prompts import PromptTemplate
-from langchain_community.chat_models import ChatOllama
-from langchain_community.embeddings import OllamaEmbeddings
+from langchain_ollama import ChatOllama
+from langchain_ollama import OllamaEmbeddings
 from pydantic import BaseModel, Field
 
 from utils.citations import CitationFormatter
